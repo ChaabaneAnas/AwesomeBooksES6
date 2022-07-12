066fc10253260b6475eee79c0a5704c4b0d032ab
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-07-12 17:52:18 +0100

    date and time

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {ui} from "./moduels/ui.js";
-import {store} from "./moduels/store.js"
+import {ui} from "./modules/ui.js";
+import {store} from "./modules/store.js";
 
 class Books {
   constructor(Title, Author){
@@ -24,3 +24,29 @@ remove.addEventListener('click', (e) => {
   location.reload();
 })
 
+const List = document.querySelector('.List');
+const Add = document.querySelector('.Add');
+const Contact = document.querySelector('.Contact');
+
+Add.addEventListener('click', () => {
+  document.querySelector('.book-list-container').style.display = 'none';
+  document.querySelector('.input').style.display = 'block';
+  document.querySelector('.contact').style.display = 'none';
+});
+
+List.addEventListener('click', () => {
+  document.querySelector('.book-list-container').style.display = 'block';
+  document.querySelector('.input').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
+  location.reload();
+});
+
+Contact.addEventListener('click', () => {
+  document.querySelector('.book-list-container').style.display = 'none';
+  document.querySelector('.input').style.display = 'none';
+  document.querySelector('.contact').style.display = 'block';
+});
+
+const displaytime = document.querySelector('.time'); 
+const shownDate = [Date().split(' ').splice(1, 4).join(' ')];
+displaytime.append(shownDate);
