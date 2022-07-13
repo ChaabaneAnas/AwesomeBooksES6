f5ac07141f61cf1668b92f39e3225e3470651b7f
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-07-13 08:21:56 +0100

    main

--- modules/luxon.js
+++ modules/luxon.js
@@ -8527,17 +8527,6 @@ var luxon = (function (exports) {
 
     return DateTime;
   }();
-  function friendlyDateTime(dateTimeish) {
-    if (DateTime.isDateTime(dateTimeish)) {
-      return dateTimeish;
-    } else if (dateTimeish && dateTimeish.valueOf && isNumber(dateTimeish.valueOf())) {
-      return DateTime.fromJSDate(dateTimeish);
-    } else if (dateTimeish && typeof dateTimeish === "object") {
-      return DateTime.fromObject(dateTimeish);
-    } else {
-      throw new InvalidArgumentError("Unknown datetime argument: " + dateTimeish + ", of type " + typeof dateTimeish);
-    }
-  }
 
   var VERSION = "3.0.1";
 
